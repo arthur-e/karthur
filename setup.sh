bda1a00d2254159e682ae68c5b9f80fb1d97d213
K. Arthur Endsley <endsley@umich.edu>
2016-06-20 14:47:53 -0400

    Theme changes and changes to setup.sh; added .gitignore file

--- setup.sh
+++ setup.sh
@@ -1,19 +1,26 @@
-virtualenv -p /usr/bin/python3.4 /usr/local/pythonenv/pelican-env
+virtualenv -p /usr/bin/python3.5 /usr/local/pythonenv/pelican-env
 
 # Installing pelican-plugins repo
 cd /usr/local/
-sudo mkdir pelican && sudo chown arthur pelican && cd pelican
+sudo mkdir pelican
+sudo chown arthur pelican
+cd pelican
 git clone https://github.com/getpelican/pelican-plugins
 
 # Installing support for the thumbnailer plug-in
 cd /usr/local/
-sudo mkdir openjpeg && sudo chown arthur openjpeg && cd openjpeg
+sudo mkdir openjpeg
+sudo chown arthur openjpeg
+cd openjpeg
 wget http://downloads.sourceforge.net/project/openjpeg.mirror/2.1.0/openjpeg-2.1.0.tar.gz
 tar -xzvf openjpeg-2.1.0.tar.gz && rm openjpeg-2.1.0.tar.gz && cd openjpeg-2.1.0
 cmake .
-make -j 4
+make
 sudo make install
 sudo ldconfig
 
 source /usr/local/pythonenv/pelican-env/bin/activate
-pip install -I Pillow
+pip install -r /usr/local/dev/karthur/REQUIREMENTS
+
+# Get fonts
+rsync -avc karthur.org:/var/www/karthur/theme/fonts /usr/local/dev/karthur/themes/karthur/static/
