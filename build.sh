118c5b9e627e1b578b0c6709d2cb0f717c3708be
K. Arthur Endsley <endsley@umich.edu>
2018-02-19 22:11:44 -0500

    Added note to build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ pelican content
 # Remove raw TeX from CV
 sed -i "s/\\\hfill//g" output/pages/cv.html
 
+# TODO Recursively replace "---" with "&#8212;" to fix em dashes
+
 # Host a server in the output directory
 cd output/
 python -m http.server
