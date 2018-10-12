e656491a7a3364bfbcf3127f29b7fb25924f1c03
K. Arthur Endsley <endsley@umich.edu>
2018-10-12 13:19:29 -0400

    Copied sed rules from build.sh to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ pelican content -s publishconf.py
 
 # Remove raw TeX from CV
 sed -i "s/\\\hfill//g" output/pages/cv.html
+sed -i -E "s/\\\setlength.+parindent.+//g" output/pages/cv.html
+sed -i -E "s/\\\hangindent=.em//g" output/pages/cv.html
+sed -i -E "s/\\\pubgroup.+//g" output/pages/cv.html
 
 rsync -avc --delete output/ karthur.org:/var/www/karthur/
 rsync -avc content/ karthur.org:/home/arthur/Blog
