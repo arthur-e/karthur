b2e29dd10773569249c92d8c3d400c338dcddf72
K. Arthur Endsley <endsley@umich.edu>
2018-10-12 13:06:48 -0400

    Updated styles for CV page: hanging indents on Publications

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ pelican content
 
 # Remove raw TeX from CV
 sed -i "s/\\\hfill//g" output/pages/cv.html
+sed -i -E "s/\\\setlength.+parindent.+//g" output/pages/cv.html
+sed -i -E "s/\\\hangindent=.em//g" output/pages/cv.html
+sed -i -E "s/\\\pubgroup.+//g" output/pages/cv.html
 
 # TODO Recursively replace "---" with "&#8212;" to fix em dashes
 
