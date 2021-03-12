e42a09ee90d70d95cb72c7777ad52e41f7671e7e
K. Arthur Endsley <endsley@umich.edu>
2021-03-12 08:13:54 -0500

    Seemed to need to remove --delete arg from rsync to prevent deletion of local files
    Throwing my hands up here. I don't know why it isn't behaving as expected.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ pelican content -s publishconf.py
 # Remove raw TeX from HTML output
 . ./clean.sh
 
-rsync -avc --delete output/ $1@karthur.org:/var/www/karthur/
+rsync -avc output/ $1@karthur.org:/var/www/karthur/
 rsync -avc content/ $1@karthur.org:/home/arthur/Blog
