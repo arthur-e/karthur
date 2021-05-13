cd447bfa36b39020fc7e96d723896c34f44479d7
K. Arthur Endsley <endsley@umich.edu>
2021-05-13 10:59:22 -0400

    Updated recent publications

--- pelicanconf.py
+++ pelicanconf.py
@@ -56,13 +56,13 @@ LINKS = (('Michigan Tech Research Institute', 'http://www.mtri.org/'),
 
 # Should be (date, title, URL, journal name)
 PUBLICATIONS = (
+    ('2021-05', 'Impacts of climate and wildfire on GPP...in Alaska', 'https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2020JG006078', 'JGR: Biogeosciences'),
     ('2020-12', 'Satellite monitoring of global surface soil organic carbon...', 'https://agupubs.onlinelibrary.wiley.com/doi/abs/10.1029/2020JG006100', 'JGR: Biogeosciences'),
-    ('2020-12', 'Monitoring crop status...using the SMAP Level 4 Carbon product', 'https://www.frontiersin.org/articles/10.3389/fdata.2020.597720/abstract', 'Frontiers in Big Data'),
-    ('2019-01', '...Soil moisture monitoring...in a mountain watershed', 'https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2018WR023653', 'Water Resources Research'))
+    ('2020-12', 'Monitoring crop status...using the SMAP Level 4 Carbon product', 'https://www.frontiersin.org/articles/10.3389/fdata.2020.597720/abstract', 'Frontiers in Big Data'),)
 
 # Social widget
 SOCIAL = (('GitHub', 'http://github.com/arthur-e/'),
-    ('BitBucket', 'http://bitbucket.org/kaendsle'),
+    ('BitBucket', 'http://bitbucket.org/endsley'),
     ('RSS', '/feeds/blog.atom.xml'),)
 
 DEFAULT_PAGINATION = 10
