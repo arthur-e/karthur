6cc2be549d1790b3fee3ed7a64bd30e90869028f
K. Arthur Endsley <endsley@umich.edu>
2021-12-09 11:42:21 -0700

    Added removal of newpage decs from output HTML

--- clean.sh
+++ clean.sh
@@ -1,7 +1,9 @@
 # Remove raw TeX from CV
+sed -i "s|\\\newpage||g" output/pages/cv.html
 sed -i "s/\\\hfill//g" output/pages/cv.html
 sed -i -E "s/\\\setlength.+parindent.+//g" output/pages/cv.html
 sed -i -E "s/\\\hangindent=.em//g" output/pages/cv.html
 sed -i -E "s/\\\pubgroup.+\}//g" output/pages/cv.html
+sed -i -E "s/\\\newpage\}//g" output/pages/cv.html
 
 # TODO Recursively replace "---" with "&#8212;" to fix em dashes
